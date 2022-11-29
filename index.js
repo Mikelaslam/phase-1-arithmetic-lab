6a0f3d182c01c9a0c3c7bc87354fb737d5e83a3f
mikelaslam <kipkorir385@gmail.com>
2022-11-29 12:20:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+let num1 = 31;
+let num2 = 2;{
+    const multiply = num1*num2  
+}
+{
+const random = Math.floor(Math.random() *6) +2;  
+}  
+{   
+        let num3 = 16;   
+        let num4 = 6;  {
 
-// Write your code here
+        const mod = num3%num4;  
+        }
+{
+        const max = Math.max(7,11,20);   
+}
+
+}
